c133d2015830a8a2574cab30ff73e306642ce3ab
ompiepy <ompie.py@gmail.com>
2023-07-04 18:12:41 +0545

    again

--- app/doece/ourteam/faculty/layout.jsx
+++ app/doece/ourteam/faculty/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <>
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
@@ -21,6 +21,6 @@ export default function RootLayout({ children }) {
         <HeaderComponent menuItems={menuItems} />
         <main>{children}</main>
       </body>
-    </html>
+    </>
   );
 }
