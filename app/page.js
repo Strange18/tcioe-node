6ab734f6b7a8712f7ec60a337431e2a54dd53af3
Nishan Khanal <khanal.nishan28@gmail.com>
2023-07-05 01:52:15 +0545

    added academic programs pages and added reports

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import AboutThapathaliComponent from "@/components/AboutThapathaliComponent";
 import CampusChiefComponent from "@/components/CampusChiefComponent";
 import CampusImageGallery from "@/components/CampusImageGallery";
@@ -8,31 +8,37 @@ import StudentTestimonials from "@/components/StudentTestimonials";
 
 //paxi backend bata data fetch garera pathaune
 import hero from "@/assets/Hero.png";
-import CampusCheifimage from '../assets/CC_image.png'
+import CampusCheifimage from "../assets/CC_image.png";
 import HeaderComponent from "@/components/HeaderComponent";
 import { menuItems } from "@/utils/menuItems";
 import FooterComponent from "@/components/FooterComponent";
 import SubNavBar from "@/components/SubNavBar";
 const data = {
-  post: "Campus chief",
-  name: "Dr. Prof. Khem Gyawali",
-  message: " Welcome to our campus! As the Campus Head, I am delighted to have you join our vibrant community. Our campus is a place where students are encouraged to explore their passions, expand their horizons, and create lasting memories. We strive to provide a nurturing and inclusive environment that fosters academic excellence, personal growth, and holistic development. With state-of-the-art facilities, dedicated faculty members, and a wide range of co-curricular activities, we aim to empower our students to become future leaders and make a positive impact in their chosen fields.",
+  post: "Campus Chief",
+  name: "Dr. Khem Gyanwali",
+  message:
+    " Welcome to our campus! As the Campus Head, I am delighted to have you join our vibrant community. Our campus is a place where students are encouraged to explore their passions, expand their horizons, and create lasting memories. We strive to provide a nurturing and inclusive environment that fosters academic excellence, personal growth, and holistic development. With state-of-the-art facilities, dedicated faculty members, and a wide range of co-curricular activities, we aim to empower our students to become future leaders and make a positive impact in their chosen fields.",
   photo: CampusCheifimage,
-}
+};
 
 export default function Home() {
   return (
     <>
-    <SubNavBar/>
-      <HeaderComponent menuItems={menuItems}/>
+      <SubNavBar />
+      <HeaderComponent menuItems={menuItems} />
       <div>
         <HeroComponent img={hero} />
-        <CampusChiefComponent post={data.post} name={data.name} photo={data.photo} message={data.message} />
+        <CampusChiefComponent
+          post={data.post}
+          name={data.name}
+          photo={data.photo}
+          message={data.message}
+        />
         <AboutThapathaliComponent />
         <OurDepartmentsSection />
         <CampusImageGallery />
         <StudentTestimonials />
       </div>
     </>
-  )
+  );
 }
