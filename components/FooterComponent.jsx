be985040765f64f5a4341d545949eb3ffd352b1d
Prateek Poudel <86917704+Prateek61@users.noreply.github.com>
2023-07-19 22:36:22 +0545

    Prateek61/home page fixes (#2)
    * Added links to department in Our Departments section
    
    * added link to mail in footer
    
    * Added link to google map in location in footer section

--- components/FooterComponent.jsx
+++ components/FooterComponent.jsx
@@ -213,7 +213,7 @@ const FooterComponent = () => {
               alt="icon"
               color="#ffffff"
             />
-            <ContactText>info@tcioe.edu.np</ContactText>
+            <ContactText><a href="mailto: info@tcioe.edu.np">info@tcioe.edu.np</a></ContactText>
           </ContactItem>
           <ContactItem>
             <Image
@@ -223,7 +223,7 @@ const FooterComponent = () => {
               alt="icon"
               color="#ffffff"
             />
-            <ContactText>Thapathali, Nepal</ContactText>
+            <ContactText><a href="https://goo.gl/maps/gEUHf6kqwdPoWe488" target="_blank">Thapathali, Nepal</a></ContactText>
           </ContactItem>
         </Contact>
       </Container>
