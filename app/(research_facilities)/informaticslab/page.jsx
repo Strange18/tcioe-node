a5b7ddc652f5faf06c82d3d438ad25070ed6ca36
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-07-19 22:02:50 +0545

    tuact remaining

--- app/(research_facilities)/informaticslab/page.jsx
+++ app/(research_facilities)/informaticslab/page.jsx
@@ -1,42 +1,16 @@
-// import AboutComponent from "../AboutComponent";
-// import energy_lab_img from "../../../assets/research_facilities/energy_lab.png";
+import AboutComponent from "../AboutComponent";
+import informatics_lab_img from "../../../assets/research_facilities/informatics_lab.jpg";
 
-// const data = {
-//   heading: "Informatics Lab",
-//   body_text: "The Department of Automobile and Mechanical Engineering at Thapathali Campus has actively engaged students in extracurricular activities focused on energy conservation and electric vehicles. By organizing energy audits and promoting the use of electric vehicles, students gained valuable knowledge about energy conservation and the benefits of electric transportation. These initiatives not only developed practical skills but also raised awareness about sustainable energy solutions. The Energy System Research Laboratory (ESRL) within the department has emerged as a leading research facility, conducting groundbreaking research on energy modeling in South Asia. Under the guidance of Assistant Professor Dr. Khem Gyanwali, the laboratory's research assistants have made significant contributions to the field. Ongoing projects at ESRL are advancing our understanding of renewable energy, energy efficiency, and energy storage. The Energy Modeling Research Group is exploring innovative ways to harness renewable energy, while the Advanced Computation Research Group is developing neural network models for various applications. Future projects include locating optimal EV charging stations and developing guidelines for transforming conventional vehicles into electric ones. With dedicated faculty members and promising projects, the Energy System Research Lab is poised for continued success.",
-//   img: energy_lab_img,
-// };
-
-// export default function EnergyLab() {
-//   return (
-//     <>
-//       <AboutComponent heading={data.heading} body_text={data.body_text} img={data.img} />
-//     </>
-//   );
-// }
-
-
-
-
-'use client';
-
-import styled from 'styled-components'
-
-const Heading = styled.h1`
-
-text-align: center;
-margin: 8rem 0;
-color: #20068E;
-
-
-`;
-
-const Page = () => {
-  return (
-    <>
-    <Heading>Work in Progress...!</Heading>
-    </>
-  );
+const data = {
+  heading: "Informatics Lab",
+  body_text: "As a center of excellence in informatics, our lab is at the forefront of technological innovation and research. We are dedicated to exploring the dynamic world of information science, data analysis, and computational intelligence. Equipped with cutting-edge technology and supported by a team of skilled experts, we delve into diverse areas, including artificial intelligence, machine learning, data mining, and bioinformatics.",
+  img: informatics_lab_img,
 };
 
-export default Page;
+export default function EnergyLab() {
+  return (
+    <>
+      <AboutComponent heading={data.heading} body_text={data.body_text} img={data.img} />
+    </>
+  );
+}
