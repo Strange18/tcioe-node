f7f3fd2f3d43025124a790f9a99fc2955c3bcbaf
Nishan Khanal <khanal.nishan28@gmail.com>
2023-07-19 07:29:27 +0545

    removed fitt from menubar

--- utils/departmentmenuitems_doame.js
+++ utils/departmentmenuitems_doame.js
@@ -1,37 +1,36 @@
 export const menuItems = [
-    {
-        title: "Home",
+  {
+    title: "Home",
+    url: "/doame",
+  },
+  {
+    title: "Our Team",
+    url: "/doame",
+    submenu: [
+      {
+        title: "Faculty",
         url: "/doame",
-    },
-    {
-        title: "Our Team",
+      },
+      {
+        title: "Staffs",
         url: "/doame",
-        submenu: [
-            {
-                title: "Faculty",
-                url: "/doame"
-            },
-            {
-                title: "Staffs",
-                url: "/doame"
-            },
-           
-        ]
-    },
-    {
-        title: "Programs",
-        url: "/doame",
-    },
-    {
-        title: "Activity",
-        url: "/doame",
-    },
-    {
-        title: "Notices",
-        url: "/doame",
-    },
-    {
-        title: "Resources",
-        url: "/doame",
-    },
-]
+      },
+    ],
+  },
+  {
+    title: "Programs",
+    url: "/doame",
+  },
+  {
+    title: "Activity",
+    url: "/doame",
+  },
+  {
+    title: "Notices",
+    url: "/doame",
+  },
+  {
+    title: "Resources",
+    url: "/doame",
+  },
+];
