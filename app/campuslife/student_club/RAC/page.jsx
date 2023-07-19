15eb2de91b8b8353e57882fcc8f35a00d2aad4fb
jyotsna-jha <116620336+jyotsna-jha@users.noreply.github.com>
2023-07-19 09:45:07 +0545

    changed texts of rac page

--- app/campuslife/student_club/RAC/page.jsx
+++ app/campuslife/student_club/RAC/page.jsx
@@ -5,10 +5,25 @@ import RacImageGallery from "@/components/RacGallary";
 
 const page = () => {
   const text = [
-    `The Robotics and Automation Center (RAC) stands as one of the oldest and most prominent organizations at Thapathali Campus, serving as the formal body of our esteemed institution. With a rich history, RAC has dedicated itself to the advancement of robotics and technology, consistently pushing the boundaries of innovation.The success of RAC is evident through its remarkable achievements in renowned competitions. From emerging victorious in IIT Techfest, a prestigious tech competition, to securing victories in Yantra, a national robotics competition, RAC has consistently proven its mettle. Moreover, their active participation in various competitions, such as soccers and robo wars, further showcases their unwavering commitment to pushing the boundaries of robotics and automation.
-    `,
+    `Robotics and Automation center (RAC) at Thapathali Campus, TU offers a wide range
+    of programs for students interested in robotics and automation. The center provides courses in
+    Robotics and Automation, Control Systems, Mechatronics, and Artificial Intelligence. These
+    courses offer students practical knowledge in the field, preparing them for a career in the industry.
+    The center also provides research opportunities for students, and they are guided by experienced
+    faculty members. Students from the Automobile and Mechanical, Industrial, Electronics, and
+    Computer engineering departments work cohesively in Robotics and Automation Center for
+    various projects and competitions..
+    .`,
     `RAC's inclusive approach is reflected in its diverse membership, welcoming students from every faculty within our campus. This interdisciplinary collaboration fosters a vibrant environment where individuals from various backgrounds contribute their unique perspectives and expertise to the field of robotics.
-    .Through their tireless efforts, RAC has solidified its position as a pioneering force in the realm of robotics and technology, leaving an indelible mark on the landscape of innovation within our campus and beyond.`,
+    .Through their tireless efforts, RAC has solidified its position as a pioneering force in the realm of robotics and technology, leaving an indelible mark on the landscape of innovation within our campus and beyond.In addition to programs, the RAC at Thapathali Campus also organizes competitions and
+    training programs for students. In the academic year 2079, RAC participated, organized, and won
+    several competitions including the International Micro mouse Challenge held at IIT Bombay with
+    the first runner-up position. It also organized Robotic Week Jestha 29-3 Ashar, which was a great
+    opportunity for students to showcase their skills and knowledge in the field. Furthermore, it has
+    organized a one-week training program on Aeromodelling and PCB design and fabrication,
+    allowing students to learn and experiment with the latest technology. These programs and
+    competitions were a great way for students to gain practical experience, showcase their skills, and
+    learn from industry experts.`,
   ];
 
   return (
