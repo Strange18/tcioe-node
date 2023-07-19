4d7e188e32325113ea4e49f874ae32c120733477
Nishan Khanal <khanal.nishan28@gmail.com>
2023-07-19 19:52:37 +0545

    created staff's union page

---
+++ app/campuslife/unions/su/layout.jsx
@@ -0,0 +1,22 @@
+import FooterComponent from "@/components/FooterComponent";
+import HeaderComponent from "@/components/HeaderComponent";
+import { menuItems } from "@/utils/menuItems";
+import Head from "next/head";
+
+export const metadata = {
+  title: "Staff's Union",
+  description: "Staff's Union of thapathali campus",
+};
+
+export default function root({ children }) {
+  return (
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
+      <HeaderComponent menuItems={menuItems} />
+      {children}
+    </>
+  );
+}
