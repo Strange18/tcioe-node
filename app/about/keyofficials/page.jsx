2a02fe7d00478aaff4de92d2a2c6fd29912d54db
Nishan Khanal <khanal.nishan28@gmail.com>
2023-07-20 22:25:00 +0545

    removed authorization for fetching data

--- app/about/keyofficials/page.jsx
+++ app/about/keyofficials/page.jsx
@@ -1,51 +1,23 @@
 "use client";
-
 import { RenderTeamCards } from "@/components/RenderTeamCards";
 import React, { useEffect, useState } from "react";
 
-const fetchData = async (token) => {
+const fetchData = async () => {
   const res = await fetch(
-    "https://notices.tcioe.edu.np/api/department/staffmembers/",
-    {
-      headers: { Authorization: `Bearer ${token}` },
-    }
+    "https://notices.tcioe.edu.np/api/department/staffmembers/"
   );
   const data = await res.json();
   return data;
 };
-
 const Page = () => {
-  const [accessToken, setAccessToken] = useState(null);
   const [keyOfficials, setKeyOfficials] = useState([]);
   useEffect(() => {
-    console.log(process.env.NEXT_PUBLIC_USERNAME);
-    const authenticate = async () => {
-      const res = await fetch("https://notices.tcioe.edu.np/api/token/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: process.env.NEXT_PUBLIC_USERNAME,
-          password: process.env.NEXT_PUBLIC_PASSWORD,
-        }),
-      });
-      const { access, refresh } = await res.json();
-      setAccessToken(access);
+    const getData = async () => {
+      const data = await fetchData();
+      setKeyOfficials(data);
     };
-    authenticate();
+    getData();
   }, []);
-
-  useEffect(() => {
-    if (accessToken) {
-      const getData = async () => {
-        const data = await fetchData(accessToken);
-        setKeyOfficials(data);
-      };
-      getData();
-    }
-  }, [accessToken]);
-
   let campusChief = keyOfficials.slice(0, 1);
   let members = keyOfficials.slice(1, -1);
   return (
