bd809264659d57a29f24e05b04fef294de919d48
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-11-26 22:25:03 +0545

    Layout code added

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ function RootLayout({ children }) {
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
